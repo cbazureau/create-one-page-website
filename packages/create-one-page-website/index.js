4244699fcfa427bbe4721fe015b0f2bd3edaf7d6
Cédric Bazureau <cedric.bazureau@renault.com>
2021-06-08 21:07:28 +0200

    core(create-one-page-website): improve script

--- packages/create-one-page-website/index.js
+++ packages/create-one-page-website/index.js
@@ -2,6 +2,7 @@
 
 /**
  * Copyright (c) 2021-present, Cédric Bazureau.
+ * Mostly inspired by https://github.com/facebook/create-react-app
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
